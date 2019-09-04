3486ffb4812123f09f581c279e358359b51d859c
Katherine <kmazzolini411@gmail.com>
2019-09-03 20:48:29 -0500

    Add Check Files App
    Added gui and main files for this application.

---
+++ Check_Files_App/check_files_func.py
@@ -0,0 +1,51 @@
+##Python 3.7.3
+#
+#
+#Author: Katherine Mazzolini
+#Tested on Mac OS High Sierra v.10.13.6
+
+
+from tkinter import *
+import tkinter as tk
+
+import check_files_main
+import check_files_func
+
+
+#list of funcitons needed
+#
+#
+#def center_window(self)
+#def browse1(self)
+#def browse2(self)
+#def fileCheck(self)
+#def close(self)
+#def ask_quit(self)
+#def txt1(self)
+#def txt2(self)
+
+
+
+
+
+
+if __name__ == "__man__":
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+
